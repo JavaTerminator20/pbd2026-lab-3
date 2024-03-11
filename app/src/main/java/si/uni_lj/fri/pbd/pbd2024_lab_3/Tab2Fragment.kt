1c52d83f33202447ca4fa47164d43d5f99eac9e5
Octavian Machidon <octavian@Octavians-MacBook-Pro.local>
2024-03-11 11:40:07 +0100

    initial version

---
+++ app/src/main/java/si/uni_lj/fri/pbd/pbd2024_lab_3/Tab2Fragment.kt
@@ -0,0 +1,29 @@
+package si.uni_lj.fri.pbd.pbd2024_lab_3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class Tab2Fragment : Fragment() {
+    private var recyclerView: RecyclerView? = null
+    private var layoutManager: RecyclerView.LayoutManager? = null
+    private var adapter: RecyclerView.Adapter<*>? = null
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_tab2, container, false)
+        recyclerView = view.findViewById(R.id.recycler_view)
+        layoutManager = LinearLayoutManager(view.context)
+        recyclerView?.layoutManager = layoutManager
+        adapter = RecyclerAdapter()
+        recyclerView?.adapter = adapter
+        return view
+    }
+}
